d247d768ed696712cf3ef0e64fbe3997e205e98d
Rem Jassai <60391009+remjassai@users.noreply.github.com>
2023-03-07 22:41:04 +0100

    Primi

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,16 @@ public class Main {
         Dessert cremaCatalanaSuTeschioDiTasso = new Dessert("Crema Catalana su teschio di Tasso",8.00);
         Dessert zuccheroFilatoSuPeroneDiPuma = new Dessert("Zucchero filato su perone di Puma",14.00);
         System.out.println("Le bevande sono : " + Dessert.getTuttiIDessert());
+
+        //PRIMI
+        Primi pappardelleAlRaguDiCinghiale = new Primi("Pappardelle al ragù di Cinghiale",6.5);
+        Primi gnocchettiAlRisoNeroConCarpaccioDiVitelloCrudoEPeperoni = new Primi("Gnocchetti al riso nero con carpaccio di Vitello crudo e peperoni",7.50);
+        Primi moussaka = new Primi("Moussaka",4.50);
+        Primi ravioliDiPastaFrescaConRipienoDiVitelloCaprinoEMarsala = new Primi("Ravioli di pasta fresca con ripieno di Vitello, caprino e marsala",11.00);
+        Primi fagioliConCostineAffumicate = new Primi("Fagioli con costine affumicate",7.50);
+        Primi canederliAllaTirolese = new Primi("Canederli alla tirolese",9.50);
+        Primi tortelliniInBrodoDiCappone = new Primi("Tortellini in brodo di Cappone",12.00);
+        Primi ciorbaDiCapraTransilvanaAllaSgarbi = new Primi("Ciorba di Capra transilvana alla Sgarbi",10.00);
+        System.out.println("I primi sono : " + Primi.getTuttiIPrimi());
     }
 }

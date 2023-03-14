e31c2db31c665649485a62075d2eecb4e7accba0
Shpend <shpend.devel.kryeziu@gmail.com>
2023-03-14 18:51:53 +0100

    finalize: color format

--- src/Main.java
+++ src/Main.java
@@ -10,6 +10,8 @@ public class Main {
     public static final String ANSI_PURPLE_BACKGROUND = "\u001B[45m";
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
+    public static final String ANSI_RED_FONT = "\u001B[31m";
+    public static final String ANSI_RESET = "\u001B[0m";
 
     public static void main(String[] args) {
         System.out.println("|_-'-_| Ristorante Balkan Delish |_-'-_|");
@@ -17,10 +19,10 @@ public class Main {
         System.out.println("Cucina di carne");
         Menu menuDiCarne = new Menu();
 
-        System.out.println(ANSI_CYAN_BACKGROUND + menuDiCarne.getBevandeList() + "\u001B[0m");
-        System.out.println(ANSI_RED_BACKGROUND + menuDiCarne.getPrimiList() + "\u001B[0m");
-        System.out.println(ANSI_GREEN_BACKGROUND + menuDiCarne.getSecondiList() + "\u001B[0m");
-        System.out.println(ANSI_YELLOW_BACKGROUND + menuDiCarne.getDessertList() + "\u001B[0m");
-        System.out.println("\u001B[31m" + menuDiCarne.prezzoMedio() + "\u001B[0m");
+        System.out.println(ANSI_CYAN_BACKGROUND + menuDiCarne.getBevandeList() + ANSI_RESET);
+        System.out.println(ANSI_RED_BACKGROUND + menuDiCarne.getPrimiList() + ANSI_RESET);
+        System.out.println(ANSI_GREEN_BACKGROUND + menuDiCarne.getSecondiList() + ANSI_RESET);
+        System.out.println(ANSI_YELLOW_BACKGROUND + menuDiCarne.getDessertList() + ANSI_RESET);
+        System.out.println(ANSI_RED_FONT + menuDiCarne.prezzoMedio() + ANSI_RESET);
     }
 }

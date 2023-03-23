f0fe9477e5fcffdca62dbd1ad51d781dabd236a3
albilug <alberto.lugato@gmail.com>
2023-03-23 10:56:19 +0100

    Aggiornati Dessert con metodi antisgamo

--- src/Dessert.java
+++ src/Dessert.java
@@ -2,6 +2,9 @@ public class Dessert extends Portata {
     private int glycemicIndex;
     public Dessert(String name, double price, int glycemicIndex, Tipologia tipologia) {
         super(name, price, tipologia);
+        if (glycemicIndex < 0 || glycemicIndex > 100) {
+            throw new IllegalArgumentException("Glycemic index must be between 0 and 100.");
+        }
         this.glycemicIndex = glycemicIndex;
     }
 
@@ -11,7 +14,7 @@ public class Dessert extends Portata {
     //TODO se usiamo campi privati i getter e i setter devono essere puri
     public void setGlycemicIndex(int glycemicIndex) {
         if (glycemicIndex>100 || glycemicIndex<0) {
-            System.out.println("ERRORE: Glycemic Index value must be between 0 and 100");
+            throw new IllegalArgumentException("Glycemic index must be between 0 and 100.");
         } else {this.glycemicIndex = glycemicIndex;}
     }
 

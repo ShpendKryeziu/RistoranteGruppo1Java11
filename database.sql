0d2896ce08034db86dd39d3baf99d2e87ef9cbfd
michelepc <mangiacotti.fi@gmail.com>
2023-05-03 09:55:12 +0200

    code review 03/05

---
+++ database.sql
@@ -0,0 +1,5 @@
+ognuno avrà il suo database e qui mettiamo la creazione delle tabelle e l' eveuntuali modifiche post creazione
+quindi ogni persona che inizierà a lavorare lancerà questo script per avere la struttura base
+
+---------------------------------------------------------------------
+

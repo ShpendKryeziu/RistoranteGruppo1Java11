709d6be229914efa8ef678e80008537e8cb481d0
Keicabba <peppino_cbr@hotmail.com>
2023-05-10 19:04:33 +0200

    adeguato classe "Secondo" con l'inserimento di allergeni e annotazioni inserito nel main gli allergeni dei secondi e aggiunto arraylist di allergeni "piottino,nodini,carni,budini"

--- src/portate/Secondo.java
+++ src/portate/Secondo.java
@@ -2,13 +2,39 @@ package portate;
 
 import enumerators.*;
 
+import java.util.ArrayList;
+/**
+ * Classe Secondo
+ * Crea secondi piatti
+ */
 public class Secondo extends Portata {
 
-    //TODO sistamare
-    public Secondo(String name, double price) { super(name, price, TipoPortataEnum.SECONDO); }
+    private ArrayList<AllergensEnum> allergens;
 
+    /**
+     * Crea un nuovo oggetto di tipo Cibo.Primo
+     *
+     * @param name      il nome del piatto
+     * @param price     il costo del piatto
+     * @param allergens la presenza di allergeni
+     */
+    public Secondo(String name, double price, ArrayList<AllergensEnum> allergens) {
+        super(name, price, TipoPortataEnum.SECONDO);
+        this.allergens = allergens;
+    }
+    public ArrayList<AllergensEnum> getAllergens() {
+        return allergens;
+    }
+
+    /**
+     * Stampa info del secondo dalla classe portata con la lista di allergeni.
+     */
     @Override
-    public String toString() {
-        return getName() + " - €" + getPrice();
+    public void printInfo() {
+        System.out.print(getName() + " --- €" + getPrice() + "\n" + TextStyleEnum.ANSI_GREY.getValue() + TextStyleEnum.ANSI_ITALIC.getValue() + " allergeni = " + TextStyleEnum.ANSI_RESET.getValue());
+        for (AllergensEnum allergen : allergens) {
+            System.out.print(TextStyleEnum.ANSI_GREY.getValue() + TextStyleEnum.ANSI_ITALIC.getValue() + allergen.getName() + "; " + TextStyleEnum.ANSI_RESET.getValue());
+        }
+        System.out.println();
     }
 }
